ea082f42bf91b949fb7812048d752c7c52ac0522
Victor kamau <Victor Wangari>
2024-03-19 09:21:21 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+ if(distance <= 400 ){
+  return 'This one is on me!'
+ } 
+ else if(distance <= 2000){
+  return 'That will be twenty bucks.'
+ }
+ else if(distance <= 2500){
+  return 'I will gladly take your thirty bucks.'
+ }
+ else if(distance > 2500){
+  return 'No can do.'
+ }
+
+
+
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.': 'No go.';
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'  
+    default:
+      return 'Bye.'  
+  } 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+
 }
